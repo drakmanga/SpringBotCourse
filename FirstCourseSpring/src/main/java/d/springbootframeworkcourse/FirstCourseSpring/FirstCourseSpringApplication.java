98571fc15525ff1ac2b2adbde076ba818b674039
drakmanga <112736413+drakmanga@users.noreply.github.com>
2024-10-09 10:51:53 +0200

    first upload

---
+++ FirstCourseSpring/src/main/java/d/springbootframeworkcourse/FirstCourseSpring/FirstCourseSpringApplication.java
@@ -0,0 +1,13 @@
+package d.springbootframeworkcourse.FirstCourseSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCourseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCourseSpringApplication.class, args);
+	}
+
+}
---
+++ FirstCourseSpring/src/test/java/d/springbootframeworkcourse/FirstCourseSpring/FirstCourseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package d.springbootframeworkcourse.FirstCourseSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCourseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
